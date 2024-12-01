30c917f0c0a45a642543c7a78b663577158273cf
Martijn <garm@ilumeo.com>
2024-12-01 21:54:56 +0100

    Fix input fetching

--- fetch-input.sh
+++ fetch-input.sh
@@ -1,2 +1,2 @@
-mkdir $DAY
-curl "https://adventofcode.com/2024/day/$DAY/input" --header "Cookie: $(cat .token)" > $DAY/input.txt
+mkdir -p $DAY
+curl "https://adventofcode.com/2024/day/$DAY/input" --header "Cookie: session=$(cat .token)" > $DAY/input.txt
